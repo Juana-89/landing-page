1224164d1388b8609992907c4c7ee9d5e920ce9b
unknown <juana.lc@hotmail.es>
2022-11-11 20:02:58 -0500

    Adding styles in the header  component

--- landing-page/src/components/Header.jsx
+++ landing-page/src/components/Header.jsx
@@ -1,14 +1,19 @@
-import { RiShoppingBag3Line } from "react-icons/ri";
+import image from '../assets/banner.png'
 
 const Header = () => {
     return(
-        <header className="flex items-center w-full p-4">
-            <div className="bg-red-300 w-1/6 text center">
-            <RiShoppingBag3Line/>
-            <h1 className="text font-bold">Star </h1>
+        <header className='flex items-center mt-2' >
+            <div className='bg-transparent w-1/6 text center'>
+            <img class='w-64' src={image} alt='Banner'/>    
             </div>
-            <nav className="bg-red-600 flex-1">
-            <h1>nav</h1>
+            <nav className='flex sm:justify-center space-x-4'>
+            <a href='#' className='rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900'><h1>Home</h1></a>
+            <a href='#' className='rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900'><h1>About us</h1></a>
+            <a href='#' className='rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900'><h1>Services</h1></a>
+            <a href='#' className='rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900'><h1>Testimonials</h1></a>
+            <a href='#' className='rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900'><h1>Contact</h1></a>
+            <a href='#' className='rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900'><h1>Blog</h1></a>
+            
             </nav>
         </header>
     )

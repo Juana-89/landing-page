91e751c6d24a8ba99a6f475bd674c323ea51dded
Jllanjech <juana.lc@hotmail.es>
2022-11-14 15:28:27 -0500

    Adding styles in the country component

--- landing-page/src/components/Country.jsx
+++ landing-page/src/components/Country.jsx
@@ -1,7 +1,15 @@
 const Country = () => {
-    return (
-        <h1>Country</h1>
-    )
-}
+  return (
+    <section className="sm:p-16 py-10 text-center sm:max-w-4xl m-auto animate-slide-center">
+      <div className="text-3xl mx-4 sm:text-6xl mb-8">Popular Package</div>
+      <div className="texto-sm sm:text-xl">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.{" "}
+      </div>
+    </section>
+  );
+};
 
-export default Country
+export default Country;

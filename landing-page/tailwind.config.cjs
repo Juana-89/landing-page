75a8fff4c8a977364b2b70ffc973a52c97fa3b75
Jllanjech <juana.lc@hotmail.es>
2022-11-13 23:11:56 -0500

    Adding animations to the buttons of the cover component

--- landing-page/tailwind.config.cjs
+++ landing-page/tailwind.config.cjs
@@ -12,6 +12,18 @@ module.exports = {
         white: '#FFFFFF',
         black: '#000000',
         green:'#252601'
+      },
+      animation:{
+        'slide-left': 'slide 1.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+        'slide-left-two': 'slide 2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+        'slide-buttons': 'slide 2.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+      },
+      keyframes:{
+       slide: {
+                    '0%':{transform: 'translateX(-1000px)'},
+                    '100%':{transform: 'translateX(0)'},
+                  }
+
       }
     },
   },
